0d6ef3c9d4272959a4c5da9602eaf75166053016
Jovan Strugar <jovan.strugar@zuehlke.com>
2018-03-18 12:12:54 +0100

    Initial commit.

---
+++ TheShop/ShopService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace TheShop
+{
+	public class ShopService
+	{
+		private DatabaseDriver DatabaseDriver;
+		private Logger logger;
+
+		private Supplier1 Supplier1;
+		private Supplier2 Supplier2;
+		private Supplier3 Supplier3;
+		
+		public ShopService()
+		{
+			DatabaseDriver = new DatabaseDriver();
+			logger = new Logger();
+			Supplier1 = new Supplier1();
+			Supplier2 = new Supplier2();
+			Supplier3 = new Supplier3();
+		}
+
+		public void OrderAndSellArticle(int id, int maxExpectedPrice, int buyerId)
+		{
+			#region ordering article
+
+			Article article = null;
+			Article tempArticle = null;
+			var articleExists = Supplier1.ArticleInInventory(id);
+			if (articleExists)
+			{
+				tempArticle = Supplier1.GetArticle(id);
+				if (maxExpectedPrice < tempArticle.ArticlePrice)
+				{
+					articleExists = Supplier2.ArticleInInventory(id);
+					if (articleExists)
+					{
+						tempArticle = Supplier2.GetArticle(id);
+						if (maxExpectedPrice < tempArticle.ArticlePrice)
+						{
+							articleExists = Supplier3.ArticleInInventory(id);
+							if (articleExists)
+							{
+								tempArticle = Supplier3.GetArticle(id);
+								if (maxExpectedPrice < tempArticle.ArticlePrice)
+								{
+									article = tempArticle;
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			article = tempArticle;
+			#endregion
+
+			#region selling article
+
+			if (article == null)
+			{
+				throw new Exception("Could not order article");
+			}
+
+			logger.Debug("Trying to sell article with id=" + id);
+
+			article.IsSold = true;
+			article.SoldDate = DateTime.Now;
+			article.BuyerUserId = buyerId;
+			
+			try
+			{
+				DatabaseDriver.Save(article);
+				logger.Info("Article with id=" + id + " is sold.");
+			}
+			catch (ArgumentNullException ex)
+			{
+				logger.Error("Could not save article with id=" + id);
+				throw new Exception("Could not save article with id");
+			}
+			catch (Exception)
+			{
+			}
+
+			#endregion
+		}
+
+		public Article GetById(int id)
+		{
+			return DatabaseDriver.GetById(id);
+		}
+	}
+}
