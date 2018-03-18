c2c3725412abce9c41449195e2473f8983727c15
Jovan Strugar <jovan.strugar@zuehlke.com>
2018-03-18 13:09:21 +0100

    Removed suppliers from ShopService.

--- TheShop/ShopService.cs
+++ TheShop/ShopService.cs
@@ -7,17 +7,11 @@ namespace TheShop
 		private DatabaseDriver DatabaseDriver;
 		private Logger logger;
 	    private IArticleBroker articleBroker;
-		private ISupplier Supplier1;
-		private ISupplier Supplier2;
-		private ISupplier Supplier3;
 		
 		public ShopService()
 		{
 			DatabaseDriver = new DatabaseDriver();
 			logger = new Logger();
-			Supplier1 = new Supplier1();
-			Supplier2 = new Supplier2();
-			Supplier3 = new Supplier3();
             articleBroker = new ArticleBroker();
 		}
 
